1be97d69aa8979884178595625e7a783f69ebad1
feftywacky <linfeiyu14@gmail.com>
2024-02-03 21:07:55 -0500

    updated to next.js 14

--- app/layout.js
+++ app/layout.js
@@ -1,11 +1,12 @@
+import { Analytics } from '@vercel/analytics/dist/react';
 import '../styles/globals.css';
-import { Analytics } from '@vercel/analytics/react';
 
 const RootLayout = ({ children }) => (
   <html lang="en">
     <head>
-      <link rel="preconnect" href="https://stijndv.com" />
-      <link rel="stylesheet" href="https://stijndv.com/fonts/Eudoxus-Sans.css" />
+      <title>
+        Feiyu Lin
+      </title>
     </head>
     <body>
       {children}
--- app/page.js
+++ app/page.js
@@ -1,24 +1,38 @@
-import { Footer, Navbar } from '../components';
-import { About, Explore, Hero } from '../sections';
-import Skills from '../sections/Skills';
+import { Footer, Navbar } from "../components";
+import { About, Explore, Hero } from "../sections";
+import Skills from "../sections/Skills";
 
-const Page = () => {
-  return (
-    <>
-      <div className="bg-background overflow-hidden">
-        <Navbar />
-        <div id="home" className="bg-page-gradient h-screen overflow-visible"><Hero /></div>
-        <div id="about" className="flex flex-col h-auto lg:h-screen overflow-visible"><About /></div>
-        <div id = "explore" className="flex flex-col h-auto lg:h-screen overflow-visible"><Explore /></div>
-        <div id="skills" className="flex flex-col h-auto lg:h-screen overflow-hidden sm:overflow-visible"><Skills /></div>
-        <div className="flex flex-col h-auto"><Footer /></div>
-      </div>
-    </>
-  )
-};
+const Page = () => (
+  <div className="bg-background overflow-hidden">
+    <Navbar />
+    <div id="home" className="bg-page-gradient h-screen overflow-visible">
+      <Hero />
+    </div>
+    <div
+      id="about"
+      className="flex flex-col h-auto lg:h-screen overflow-visible"
+    >
+      <About />
+    </div>
+    <div
+      id="explore"
+      className="flex flex-col h-auto lg:h-screen overflow-visible"
+    >
+      <Explore />
+    </div>
+    <div
+      id="skills"
+      className="flex flex-col h-auto lg:h-screen overflow-hidden sm:overflow-visible"
+    >
+      <Skills />
+    </div>
+    <div className="flex flex-col h-auto">
+      <Footer />
+    </div>
+  </div>
+);
 
 export default Page;
-
 
 /*
 svg skill icons
@@ -29,4 +43,3 @@ for bordered icons:
 svg: export optimized
 png: export 256x256
 */
-
